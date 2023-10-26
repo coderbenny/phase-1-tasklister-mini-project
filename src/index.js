017fd70ab913718c113128a771e2f57e56210d8d
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2023-10-26 14:56:24 +0300

    Completed Lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let form = document.querySelector("#create-task-form");
+  
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addTodo(e.target.newTaskDescription.value);
+    form.reset();
+  })
+
+})
+
+function addTodo(todo){
+  let list = document.createElement('li')
+  let btn = document.createElement('button');
+  btn.addEventListener('click', removeTodo)
+  btn.textContent = 'x'; 
+  list.textContent = `${todo} `;
+  list.appendChild(btn)
+  document.querySelector('#tasks').appendChild(list);
+}
+  
+function removeTodo(e){
+  e.target.parentNode.remove();
+}  
+
